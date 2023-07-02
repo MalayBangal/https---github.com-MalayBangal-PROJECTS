13d5f9db6dc642d2d5933d1eea30570b44921472
MalayBangal <malaybangal1@gmail.com>
2023-07-02 20:58:06 +0530

    add

---
+++ node_jobs/middlewares/index.js
@@ -0,0 +1,33 @@
+//& MIDDLEWARES
+const checkLoggedIn = (req,res,next) =>{
+    if(req.isAuthenticated()) next();
+    else res.redirect('/login');
+};
+
+const checkAdmin = (req,res,next) =>{
+    if(req.user.isAdmin) next();
+    else req.flash('error','You are not permitted to do that task');
+    res.redirect('/jobs');
+};
+const verifyUser = (req, res, next) => {
+	if (req.user.isAdmin || req.user._id.equals(req.params.id)) next();
+	else {
+		req.flash('error','You are not permitted to do that task');
+		res.redirect('back');
+	}
+};
+module.exports = {
+    checkLoggedIn,
+    checkAdmin,
+    verifyUser
+};
+
+// module.exports=checkLoggedIn = (req,res,next) =>{
+//     if(req.isAuthenticated()) next();
+//     else res.redirect('/login');
+// };
+
+// module.exports=checkAdmin = (req,res,next) =>{
+//     if(req.user.isAdmin) next();
+//     else return res.send('not permited');
+// };
